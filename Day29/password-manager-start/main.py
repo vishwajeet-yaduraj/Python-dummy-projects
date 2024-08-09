2e6e8e627d39fd8574680535773b79e23e61d4f7
Vishwajeet Nand Yaduraj <vishwajeetnandyaduraj786@gmail.com>
2024-08-09 23:56:33 +0530

    implemented error/exception handling throughout

--- Day29/password-manager-start/main.py
+++ Day29/password-manager-start/main.py
@@ -2,9 +2,11 @@ from tkinter import messagebox
 from tkinter import *
 import random
 import pyperclip
+import json
+
 
 def gen_pass():
-    password_entry.delete(0,END)
+    password_entry.delete(0, END)
     alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                  'u',
                  'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
@@ -34,22 +36,48 @@ def add_feature():
     email_entered = email_entry.get()
     password_entered = password_entry.get()
     website_entered = website_entry.get()
+    new_data = {website_entered:
+        {
+            "email": email_entered,
+            "password": password_entered,
+        }
+    }
     if email_entered == "" or password_entered == "" or website_entered == "":
         messagebox.showinfo(message="You left one or more field empty")
-        return
+    else:
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
 
-    to_add = f"{website_entered} | {email_entered} | {password_entered}"
-    is_ok = messagebox.askokcancel(title="Pop_up",
-                                   message=f"These are your details\nwebsite:{website_entered}\n"
-                                           f"email:{email_entered}\npassword:{password_entered}\n"
-                                           f"Would you like to proceed?")
-    if is_ok:
-        with open("data.txt", 'a') as file:
-            file.write(to_add)
-            file.write("\n")
-        password_entry.delete(0, END)
-        website_entry.delete(0, END)
-        email_entry.delete(0, END)
+            with open("data.json", 'w') as file:
+                json.dump(data, file, indent=4)
+        finally:
+            password_entry.delete(0, END)
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+
+
+def search():
+    to_search = website_entry.get()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="File Not Found", message="No Data File Found")
+    else:
+        try:
+            email_needed = data[to_search]["email"]
+            pass_needed = data[to_search]["password"]
+        except KeyError:
+            messagebox.showinfo(title="Warning", message=f"No records found for website: {to_search}")
+        else:
+            messagebox.showinfo(title="email/password", message=f"Email: {email_needed}\nPassword: {pass_needed}")
+            pyperclip.copy(pass_needed)
 
 
 window = Tk()
@@ -72,9 +100,12 @@ email_label.grid(column=0, row=2)
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
-website_entry = Entry(width=35)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
+
+search_button = Button(text="Search", width=14, command=search)
+search_button.grid(row=1, column=2)
 
 email_entry = Entry(width=35)
 email_entry.insert(0, "vishwajeet@xyz.com")
